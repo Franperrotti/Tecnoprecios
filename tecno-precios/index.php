754451067b01322414a5f1ecf4d334863fcb2e69
Franperrotti <franperrotti@gmail.com>
2019-04-24 14:34:27 -0300

    cambios aplicados por fran

--- tecno-precios/index.php
+++ tecno-precios/index.php
@@ -1,293 +1,147 @@
+<?php
+// serchbar
+include_once('serchbar.php');
+
+
+// navbar
+include_once('navbar.php');
+
+
+// promos
+include_once('promos.php');
+
+
+// slider
+include_once('slider.php');
+
+
+// productospaginaincio
+include_once('productosinicio.php');
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-  <link rel="stylesheet" href="./css/master.css">
-  <title>HOME</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/master.css">
+    <title>Preguntas Frecuentes</title>
 </head>
 
 <body>
-  <div class="container-fluid p-0">
-
-    <!-- serchbar -->
-    <div id="arriba">
-      <img class="imagen_logo" src="img/Logo.png" alt=""> </span>
-      <div class="buscar-caja">
-        <input type="search" name="buscar" class="buscar-txt" placeholder="Buscar..." />
-        <a class="buscar-btn">
-          <i class="fas fa-search-dollar"></i>
-        </a>
-      </div>
-      <!-- <span class="imagen_bag" > <a href=""><img src="img/bag.png" alt=""> </a> </span> -->
-      <ul calss="log_in">
-        <!-- <li>  <a href=""><img src="img/bag.png" alt=""> </a>   </li> -->
-        <li class="d-flex">
-          <a href="login.php" target="_blank" class="formato btn btn-primary btn-sm ml-auto ">Login</a>
-        </li>
-        <li class="d-flex">
-          <a href="registro.php" target="_blank" class="formato btn btn-primary btn-sm ml-auto ">Registrate</a>
-        </li>
-      </ul>
-      <!-- <img class="imagen_bag" src="img/bag.png" alt="">  -->
-
-    </div>
-
-
-    <div class="wrap">
-      <div class="search">
-        <input type="text" class="searchTerm" placeholder="Buscar...">
-        <button type="submit" class="searchButton">
-          <i class="fas fa-search-dollar"></i>
-        </button>
-      </div>
-    </div>
-
-
-    <!-- navbar -->
-    <section class="seccion_nav">
-      <nav class="navbar navbar-expand-lg navbar-dark flex_del_nav">
-        <a class="navbar-brand flecdh" href="#"></a>
-        <span class="dhrestnav">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span> </span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ">
-
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Tv</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Lava Ropas</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"> Heladera</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Aire Acondicionado</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contactanos</a>
-              </span>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></a>
-            </li>
-          </ul>
-
-        </div>
-      </nav>
-    </section>
 
 
 
-    <!-- Productos pagina principal -->
-
-    <div id="izquierda">
-      <img class="imagen_izquierda" src="img/Cyber.png" alt="">
-    </div>
+<!-- Footer -->
 
 
-    <div id="derecha">
-      <img class="imagen_derecha" src="img/tv.jpg" alt="">
-    </div>
+<footer class="page-footer font-small indigo">
 
-    <div id="derecha__abajo">
-      <img class="imagen_derecha_abajo" src="img/lava_ropas.jpg" alt="">
-    </div>
+    <!-- Footer Links -->
 
-    <!-- carurcel -->
+    <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
 
 
-    <div class="fondo_de_slider">
+    <!--<div class="col-lg-2 col-md-12 mb-4">
+         <img src="img/TecnoLogoBlanco.png" class="img-fluid" alt=""> 
+    </div>-->
 
-      <section class="carousel__w">
-        <div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active slide1">
-
-            </div>
-            <div class="carousel-item slide2">
-
-            </div>
-            <div class="carousel-item slide3">
-
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-      </section>
-
-    </div>
-
-    <!-- Productos -->
-
-    <div id="wrap">
-      <div id="columns" class="columns_4">
-        <figure>
-          <img src="img/iphone x.jpg">
-          <figcaption>Iphone XS</figcaption>
-          <span class="price">$44</span>
-          <a class="button" href="#">Comprar ahora</a>
-        </figure>
-
-        <figure>
-          <img src="img/iphone x.jpg">
-          <figcaption>Iphone XS</figcaption>
-          <span class="price">$44</span>
-          <a class="button" href="#">Comprar ahora</a>
-        </figure>
-
-        <figure>
-          <img src="img/iphone x.jpg">
-          <figcaption>Iphone XS</figcaption>
-          <span class="price">$44</span>
-          <a class="button" href="#">Comprar ahora</a>
-        </figure>
-
-        <figure>
-          <img src="img/iphone x.jpg">
-          <figcaption>Iphone XS</figcaption>
-          <span class="price">$44</span>
-          <a class="button" href="#">Comprar ahora</a>
-        </figure>
-
-        <figure>
-          <img src="img/iphone x.jpg">
-          <figcaption>Iphone XS</figcaption>
-          <span class="price">$44</span>
-          <a class="button" href="#">Comprar ahora</a>
-        </figure>
-
-        <figure>
-          <img src="img/iphone x.jpg">
-          <figcaption>Iphone XS</figcaption>
-          <span class="price">$44</span>
-          <a class="button" href="#">Comprar ahora</a>
-        </figure>
-      </div>
-    </div>
-
-    <!-- Footer -->
-
-
-    <footer class="page-footer font-small indigo">
-
-      <!-- Footer Links -->
-
-      <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
-
-
-      <div class="col-lg-2 col-md-12 mb-4">
-        <img src="imagen/TecnoLogoBlanco.png" class="img-fluid" alt="">
-      </div>
-
-      <!-- Grid row-->
-      <div class="row d-flex text-center justify-content-center mb-md-0 mb-4" id="footerLinks">
+    <!-- Grid row-->
+    <div class="row d-flex text-center justify-content-center mb-md-0 mb-4" id="footerLinks">
         <!-- ACA PEGUE TODOOOOOO-->
         <div class="col-md-2 mb-3">
-          <h6 class="text-uppercase font-weight-bold">
-            <a href="#!">INICIO</a>
-          </h6>
+            <h6 class="text-uppercase font-weight-bold">
+                <a href="index.php">INICIO</a>
+            </h6>
         </div>
         <!-- Grid column -->
 
         <!-- Grid column -->
         <div class="col-md-2 mb-3">
-          <h6 class="text-uppercase font-weight-bold">
-            <a href="#!">PRODUCTOS</a>
-          </h6>
+            <h6 class="text-uppercase font-weight-bold">
+                <a href="#!">PRODUCTOS</a>
+            </h6>
         </div>
         <!-- Grid column -->
 
         <!-- Grid column -->
         <div class="col-md-2 mb-3">
-          <h6 class="text-uppercase font-weight-bold">
-            <a href="#!">NOSOTROS</a>
-          </h6>
+            <h6 class="text-uppercase font-weight-bold">
+                <a href="#!">NOSOTROS</a>
+            </h6>
         </div>
         <!-- Grid column -->
 
         <!-- Grid column -->
         <div class="col-md-2 mb-3">
-          <h6 class="text-uppercase font-weight-bold">
-            <a href="faq.php">PREGUNTAS FRECUENTES</a>
-          </h6>
+            <h6 class="text-uppercase font-weight-bold">
+                <a href="#!">AYUDA</a>
+            </h6>
         </div>
         <!-- Grid column -->
 
         <!-- Grid column -->
         <div class="col-md-2 mb-3">
-          <h6 class="text-uppercase font-weight-bold">
-            <a href="#!">CONTACTANOS</a>
-          </h6>
+            <h6 class="text-uppercase font-weight-bold">
+                <a href="#!">CONTACTANOS</a>
+            </h6>
         </div>
 
         <!-- Grid column -->
 
-      </div>
-      <!-- Grid row-->
-      <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+    </div>
+    <!-- Grid row-->
+    <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
 
-      <!-- Grid row-->
-      <div class="row d-flex text-center justify-content-center mb-md-0 mb-4" id="footerIconos">
+    <!-- Grid row-->
+    <div class="row d-flex text-center justify-content-center mb-md-0 mb-4" id="footerIconos">
 
         <!-- Grid row-->
         <div class="row">
 
-          <!-- Grid column -->
-          <div class="col-md-12 py-5">
-            <div class="mb-5 flex-center">
+            <!-- Grid column -->
+            <div class="col-md-12 py-5">
+                <div class="mb-5 flex-center">
 
-              <!-- Facebook -->
-              <a class="fb-ic" href="https://facebook.com" target="_blank">
-                <i class="fab fa-facebook-f fa-lg mr-md-5 mr-3 fa-2x"> </i>
-              </a>
-              <!-- Twitter -->
-              <a class="tw-ic" href="https://twitter.com" target="_blank">
-                <i class="fab fa-twitter fa-lg mr-md-5 mr-3 fa-2x"> </i>
-              </a>
-              <!--Instagram-->
-              <a class="ins-ic" href="https://instagram.com" target="_blank">
-                <i class="fab fa-instagram fa-lg mr-md-5 mr-3 fa-2x"> </i>
-              </a>
+                    <!-- Facebook -->
+                    <a class="fb-ic" href="https://facebook.com" target="_blank">
+                        <i class="fab fa-facebook-f fa-lg mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <!-- Twitter -->
+                    <a class="tw-ic" href="https://twitter.com" target="_blank">
+                        <i class="fab fa-twitter fa-lg mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <!--Instagram-->
+                    <a class="ins-ic" href="https://instagram.com" target="_blank">
+                        <i class="fab fa-instagram fa-lg mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                </div>
             </div>
-          </div>
-          <!-- Grid column -->
+            <!-- Grid column -->
 
         </div>
         <!-- Grid row-->
 
-      </div>
-      <!-- Footer Elements -->
+    </div>
+    <!-- Footer Elements -->
 
-      <!-- Copyright -->
-      <div class="row d-flex text-center justify-content-center mb-md-0 mb-4" id="footerAll">TECNOPRECIOS 2019
+    <!-- Copyright -->
+    <div class="row d-flex text-center justify-content-center mb-md-0 mb-4" id="footerAll">TECNOPRECIOS 2019
         <p>All rights reserved</p>
-      </div>
-    </footer>
+    </div>
+</footer>
 
-
-
-
-  </div>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
