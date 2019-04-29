54cabd76556d7ff13853409591da1abe8db1761f
Romina Herrera <rominaileanaherrera@gmail.com>
2019-04-29 12:24:44 -0300

    correccion en recuperarpassword

--- tecno-precios/recuperarpassword.php
+++ tecno-precios/recuperarpassword.php
@@ -27,7 +27,7 @@ if($_POST){
 </head>
 
 <body>
-  <div class="container-fluid p-0 __divregistro">
+    <div class="container-fluid p-0 __divregistro">
     <?php
       if(isset($errores)):?>
         <ul class="alert alert-danger">
@@ -38,42 +38,44 @@ if($_POST){
         </ul>
       <?php endif;?>
 
-  
-    <section class="row">
-      <article class="col-12  " >
-          <h1 class="__h1form">Recuperar Contraseña</h1>
-            <form   class="form1 mt-1"  action="" method="POST" enctype= "multipart/form-data"  >
-              <label>Email:</label>
-              <input  class="formatoform btn btn-outline-primary"  name="email" type="text" id="email" value="<?=isset($errores["email"])? "":persistir("email") ;?>" placeholder="Ingrese su email"/>
-              <br>
-              <label>Nueva contraseña:</label>
-          
-              <input  class="formatoform btn btn-outline-primary"  name="password" type="password" id="password" value="" placeholder="Cree su contraseña" />
-              <br>
-              <label>Confirmar nueva contraseña:</label>
-            
-              <input  class="formatoform btn btn-outline-primary"  name="repassword" type="password" id="repassword" value="" placeholder="Repita su contraseña" />
-              <br>
-              <button class="formatoform btn btn-primary btn-lg"  type="submit" name="button">Enviar</button>
-            </form>
+      <section class="formulario row">
+
+<article class="formulario1 articulo1 col-12 col-md-12 col-lg-6">
+    <h1 class="__h1form">Recuperar Contraseña</h1>
+    <br>
+
+    <form class="form1 mt-1" action="" method="post" enctype= "multipart/form-data">
         
-      </article> 
+        <label>Email:</label>
+         <input  class="formatoform btn btn-outline-primary"  name="email" type="text" id="email" value="<?=isset($errores["email"])? "":persistir("email") ;?>" placeholder="Ingrese su email"/>
+        <br>
+        <label>Nueva contraseña:</label>
+          
+        <input  class="formatoform btn btn-outline-primary"  name="password" type="password" id="password" value="" placeholder="Cree su contraseña" />
+        <br>
+        <label>Confirmar nueva contraseña:</label>
+            
+        <input  class="formatoform btn btn-outline-primary"  name="repassword" type="password" id="repassword" value="" placeholder="Repita su contraseña" />
+         <br>
+        <button class="formatoform btn btn-primary btn-lg"  type="submit" name="button">Enviar</button>
 
-      
-      <article class="logoformulario articulo2  col-12 col-md-12 col-lg-6">
 
-        <img width="100%" id="imgform" src="img/tecnolog.png" alt="">
+    </form>
 
+</article>
 
-      </article>
-    </section>
+<article class="logoformulario articulo1 col-12 col-md-12 col-lg-6">
 
+    <img width="100%" id="imgform" src="img/tecnolog.png" alt="">
+
+</article>
+
+</section>
+
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-  </div>
-
 </body>
 
 </html>
