e10fb8206b15f5b059a5869d050204c101a9ab48
pamela streitmarter <streitmarter@hotmail.com>
2019-04-29 18:33:16 -0300

    footer y faq

--- tecno-precios/faq.php
+++ tecno-precios/faq.php
@@ -1,209 +1,185 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+// serchbar
+include_once('serchbar.php');
 
-<head>
-    <meta charset="UTF-8">
+
+// navbar
+/*  include_once('navbar.php');*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/master.css">
+    <link rel="stylesheet" href="css/faq.css">
     <title>Preguntas Frecuentes</title>
-</head>
+  </head>
+  <body >
+     <div class=" container " >
+      <div class="titulo card-header text-center justify-content-center" style="background-color: #0279f6;" >
+        <h1 style="color: #FFF;">Preguntas Frecuentes</h1>
+      </div>
 
-<body>
-
-    <!-- serchbar -->
-    <div id="arriba">
-        <img class="imagen_logo" src="img/Logo.png" alt=""> </span>
-        <div class="buscar-caja">
-            <input type="search" name="buscar" class="buscar-txt" placeholder="Buscar..." />
-            <a class="buscar-btn">
-                <i class="fas fa-search-dollar"></i>
-            </a>
-        </div>
-        <!-- <span class="imagen_bag" > <a href=""><img src="img/bag.png" alt=""> </a> </span> -->
-        <ul calss="log_in">
-            <!-- <li>  <a href=""><img src="img/bag.png" alt=""> </a>   </li> -->
-            <li class="d-flex">
-                <a href="login.php" target="_blank" class="formato btn btn-primary btn-sm ml-auto ">Login</a>
-            </li>
-            <li class="d-flex">
-                <a href="registro.php" target="_blank" class="formato btn btn-primary btn-sm ml-auto ">Registrate</a>
-            </li>
-        </ul>
-        <!-- <img class="imagen_bag" src="img/bag.png" alt="">  -->
-
-    </div>
-
-
-
-    <div class="titulo">
-        <h1>Preguntas Frecuentes</h1>
-    </div>
-
-    <div class="accordion" id="accordionExample">
+      <div class="accordion" id="accordionExample">
         <div class="card">
-            <div class="card-header" id="headingOne">
-                <h2 class="mb-0">
-                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Que metodos de pago hay?
-                    </button>
-                </h2>
+          <div class="card-header" id="headingOne">
+            <h2 class="mb-0">
+              <button class="btn btn-link " type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" >
+                ¿Como comprar en TecnoPrecios.com ? 
+              </button>
+            </h2>
+          </div>
+          <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+              <p> 
+                <strong>   
+                  Para realizar la compra desde TecnoPrecios.com deberás seguir los siguientes pasos:
+                </strong>
+              </p>
+              <ul style="list-style-type: number;">
+                  <li>Seleccioná el Login y pone tu usuario y contraseña. Si todavia no estas logeado, registrate primero.</li>
+                  <li>Seleccioná el producto elegido y hacé click en botón de ' Comprar'. La página te redirecciona al carrito de compras.</li>
+                  <li>Seleccioná la forma de entrega:
+                    <ol>
+                      a. <strong>Retiro  en Sucursal </strong>: Podés seleccionar retirar el producto en la sucursal más cercana sin ningún costo adicional.
+                      <br>
+                      b. <strong>Envío a Domicilio </strong>: Cargá los datos del domicilio en el que querés recibir tu compra. El costo de envío se calcula en base a la dirección ingresada.</li>
+                    </ol>
+                  </li>
+                  <li>Completá los datos de forma de pago, seleccionando entre las diferentes tarjetas de crédito habilitadas con sus respectivas cuotas y promociones.</li>
+                  <li>Completá los datos del titular de la tarjeta de crédito y el domicilio de facturación.</li>
+                    <ol>
+                      a. Si seleccionaste <strong>'Envío a Domicilio'</strong> podés elegir la misma dirección sin tener que completarla nuevamente.
+                      <br>
+                      b. Si seleccionaste <strong>' Retiro en Sucursal'</strong> necesitamos que completes el formulario para validad tu identidad.
+                    </ol> 
+                  <li>Tu compra se ha realizado con éxito. ¡Sólo resta que recibas o retires tu pedido, según la modalidad de entrega que hayas elegido!.</li>
+
+                  <li>Recordá que en caso de haber optado por 'Retiro en Sucursal' con fecha diferida, deberás esperar el e-mail que confirma que tu producto ya está disponible para que lo retires.</li>
+              </ul>
             </div>
-            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-                <div class="card-body">
-                    Podes pagar con efectivo, credito o debito.
-                </div>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-header" id="headingTwo">
-                <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        pregunta 2
-                    </button>
-                </h2>
-            </div>
-            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                <div class="card-body">
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi pariatur accusantium quis commodi veniam rerum. Libero reiciendis inventore omnis amet, suscipit harum numquam ipsam unde, nihil, facere excepturi repudiandae illum. </P>
-                </div>
-            </div>
+          </div>
         </div>
         <div class="card">
-            <div class="card-header" id="headingThree">
-                <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Pregunta 3
-                    </button>
-                </h2>
+          <div class="card-header" id="headingTwo">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              ¿Como me registro?
+              </button>
+            </h2>
+          </div>
+          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+              <p> <a href="https://www.facebook.com" target="_blank">Registrate</a> en TecnoPrecios.com y empezá a disfrutar de todos nuestros descuentos y promociones.</P>
             </div>
-            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                <div class="card-body">
-                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti doloribus dicta nemo, maxime similique, veritatis provident aut accusantium numquam harum sed nobis? Necessitatibus nisi illum assumenda labore iure nam voluptatum. </p>
-                </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingThree">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Cambios y Devoluciones
+              </button>
+            </h2>
+          </div>
+          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div class="card-body">
+              <p>| Políticas y condiciones generales |</p>
+              <p>TecnoPrecios te brinda 30 días de corrido para que puedas cambiar o devolver el producto. Los cambios se encuentran sujetos a disponibilidad de stock.</p>
+              <p>Será necesario que el producto esté en las mismas condiciones que se entregó: embalaje en perfectas condiciones, con accesorios y manuales completos.</p>
             </div>
-            <div class="card">
-                <div class="card-header" id="headingFour">
-                    <h2 class="mb-0">
-                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                            Pregunta 4
-                        </button>
-                    </h2>
-                </div>
-                <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-                    <div class="card-body">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti doloribus dicta nemo, maxime similique, veritatis provident aut accusantium numquam harum sed nobis? Necessitatibus nisi illum assumenda labore iure nam voluptatum. </p>
-                    </div>
-                </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingFour">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                Quiero anular una compra
+              </button>
+            </h2>
+          </div>
+          <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+            <div class="card-body">
+              <p>Para anular una compra realizada comunicate con el Centro de Atención al Cliente al 0810-444-7025.</p>
             </div>
-
-
-            <!-- Footer -->
-
-
-            <footer class="page-footer font-small indigo">
-
-                <!-- Footer Links -->
-
-                <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
-
-
-                <!--<div class="col-lg-2 col-md-12 mb-4">
-                     <img src="img/TecnoLogoBlanco.png" class="img-fluid" alt=""> 
-                </div>-->
-
-                <!-- Grid row-->
-                <div class="row d-flex text-center justify-content-center mb-md-0 mb-4" id="footerLinks">
-                    <!-- ACA PEGUE TODOOOOOO-->
-                    <div class="col-md-2 mb-3">
-                        <h6 class="text-uppercase font-weight-bold">
-                            <a href="index.php">INICIO</a>
-                        </h6>
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-2 mb-3">
-                        <h6 class="text-uppercase font-weight-bold">
-                            <a href="#!">PRODUCTOS</a>
-                        </h6>
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-2 mb-3">
-                        <h6 class="text-uppercase font-weight-bold">
-                            <a href="#!">NOSOTROS</a>
-                        </h6>
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-2 mb-3">
-                        <h6 class="text-uppercase font-weight-bold">
-                            <a href="#!">AYUDA</a>
-                        </h6>
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-2 mb-3">
-                        <h6 class="text-uppercase font-weight-bold">
-                            <a href="#!">CONTACTANOS</a>
-                        </h6>
-                    </div>
-
-                    <!-- Grid column -->
-
-                </div>
-                <!-- Grid row-->
-                <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
-
-                <!-- Grid row-->
-                <div class="row d-flex text-center justify-content-center mb-md-0 mb-4" id="footerIconos">
-
-                    <!-- Grid row-->
-                    <div class="row">
-
-                        <!-- Grid column -->
-                        <div class="col-md-12 py-5">
-                            <div class="mb-5 flex-center">
-
-                                <!-- Facebook -->
-                                <a class="fb-ic" href="https://facebook.com" target="_blank">
-                                    <i class="fab fa-facebook-f fa-lg mr-md-5 mr-3 fa-2x"> </i>
-                                </a>
-                                <!-- Twitter -->
-                                <a class="tw-ic" href="https://twitter.com" target="_blank">
-                                    <i class="fab fa-twitter fa-lg mr-md-5 mr-3 fa-2x"> </i>
-                                </a>
-                                <!--Instagram-->
-                                <a class="ins-ic" href="https://instagram.com" target="_blank">
-                                    <i class="fab fa-instagram fa-lg mr-md-5 mr-3 fa-2x"> </i>
-                                </a>
-                            </div>
-                        </div>
-                        <!-- Grid column -->
-
-                    </div>
-                    <!-- Grid row-->
-
-                </div>
-                <!-- Footer Elements -->
-
-                <!-- Copyright -->
-                <div class="row d-flex text-center justify-content-center mb-md-0 mb-4" id="footerAll">TECNOPRECIOS 2019
-                    <p>All rights reserved</p>
-                </div>
-            </footer>
-
+          </div>
+        </div>
+        
+        <div class="card">
+          <div class="card-header" id="headingFive">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+              ¿Mi compra está registrada?
+              </button>
+            </h2>
+          </div>
+          <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
+            <div class="card-body">
+              <p>Podes verificar si tú compra está registrada, ingresando a <strong>Mi cuenta</strong>. Para ingresar a Mi cuenta, tenés que registrarte. Una vez registrado, ingresá a la sección Mis compras.</P>
+              <p>En el detalle de la compra tenes los datos ingresados, así como también la opción de descargar tú factura. Si tenés problemas, podés completar el siguiente formulario.</p>
+            </div>
+          </div>
+        </div>
+        
+        <div class="card">
+          <div class="card-header" id="headingSix">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+              Cambio de contraseña
+              </button>
+            </h2>
+          </div>
+          <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionExample">
+            <div class="card-body">
+              <p><a href="https://www.facebook.com" target="_blank">Iniciá sesión</a>
+              en TecnoPrecios.com para cambiar tu contraseña.</p>
+            </div>
+          </div>
+        </div>
+        
+        <div class="card">
+          <div class="card-header" id="headingSeven">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+              Devoluciones en tarjetas de crédito y débito
+              </button>
+            </h2>
+          </div>
+          <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordionExample">
+            <div class="card-body">
+              <p> Los tiempos de acreditación pueden demorar entre 72 hs hábiles hasta 10 días corridos, dependiendo la tarjeta que hayas utilizado y contemplando el cierre de tú resumen de cuenta.</P>
+            </div>
+          </div>
+        </div>
+        
+        <div class="card">
+          <div class="card-header" id="headingEigth">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+              ¿Qué debo hacer si no me llego el e-mail de confirmacion de compra?
+              </button>
+            </h2>
+          </div>
+          <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordionExample">
+            <div class="card-body">
+              <p>Te recomendamos que revises tu casilla de correo no deseado. De no haber recibido el mail podés comunicarte con el Centro de Atención al Cliente al <strong>0810-444-7025</strong>. </P>
+            </div>
+          </div>
         </div>
     </div>
+</div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-
+  </body>
 </html>
+<?php
+
+include_once('footer.php');
+
+?>
