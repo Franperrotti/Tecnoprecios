5f3af650112c0393e85f4c6eaa48dc2beb92bb4c
romina herrera <rominaileanaherrera@gmail.com>
2019-06-12 16:15:44 -0300

    sprint 3b completo con bd exportada

--- tecno-precios/BD/tecnoprecios.sql
+++ tecno-precios/BD/tecnoprecios.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `tecnoprecios`;
- /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE DATABASE  IF NOT EXISTS `tecnoprecios` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 USE `tecnoprecios`;
 -- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
 --
@@ -10,7 +9,7 @@ USE `tecnoprecios`;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/* SET NAMES utf8 ;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -31,7 +30,7 @@ CREATE TABLE `products` (
   `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +39,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (2,'iphone.jpg','Iphone X','35000'),(3,'lavaropas.jpg','Lavarropas','28000'),(4,'tostadora.jpg','Tostadora','3500'),(5,'tv.jpg','Televisor 4k','52000');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,13 +52,13 @@ DROP TABLE IF EXISTS `users`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `avatar` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `perfil` int(11) NOT NULL,
+  `role` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*INSERT INTO `users` VALUES (1,'Romina Herrera','romina@gmail.com','5cffde348dcd6.jpg','$2y$10$qgf4iGE4sO1CXihETDj.bOSiT3S5TDzzkiSSTHm4swTeqE0qxrVT6',9);
+INSERT INTO `users` VALUES (1,'Romina','romina@gmail.com','$2y$10$2WUw5mcWPpHGZYLtx5Mb.ORaNPos3TrgkClvuMl7E810kf//Jw0sK','5d014d55d0687.jpg',9),(2,'administrador','admin@gmail.com','$2y$10$xH6Lx9IqKID.pwDUpRO0wOoWGzrXbD7/iaI8vdaLK.wuRrscNYEdO','5d014e5911aa0.jpg',9);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -80,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-10  9:41:56
+-- Dump completed on 2019-06-12 16:13:48
